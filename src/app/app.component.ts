fb5e252700da4e6ccc475cb734a8422ad8d1f562
212739845 <farid.ghanayim@ge.com>
2021-03-12 14:33:12 +0200

    add post example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,54 @@ import { Component } from '@angular/core';
   template: `
     <h1>Hello to angular!!!</h1>
     <app-parent></app-parent>
-    <ul>
-    <li *ngFor="let user of users ; index as i "> {{i+1}} : name: {{user.name}} , email : {{user.email}}</li>
-    </ul>
+    <!-- <div>{{posts | json}}</div> -->
+    <div *ngFor="let post of posts" class="test"> 
+        <p>userId: {{post.userId}}</p>
+        <p>id: {{post.id}}</p>
+        <p>title: {{post.title}}</p>
+        <p>body: {{post.body}}</p>
+    </div>
 
-    <div>{{users | json}}</div>
   `,
-  styles: []
+  styles: [
+    `
+    .test {
+      border:1px solid red;
+    }`
+  ]
 })
 export class AppComponent {
   title = 'test11';
-  users = [
+  posts = [
     {
-    name:'Alex',
-    email: 'alex@gmail.com'
-   },
-   {
-    name:'Tom',
-    email: 'Tom@gmail.com'
-   },
-   {
-    name:'Andrei',
-    email: 'Andrei@gmail.com'
-   },
-   {
-    name:'Ron',
-    email: 'Ron@gmail.com'
-   }
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "eum et est occaecati",
+      "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "nesciunt quas odio",
+      "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
 ]
 }
